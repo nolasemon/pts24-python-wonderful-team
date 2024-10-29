b22cc06d2494b7c6433f3f91f43cb6ad999b7de2
Robert Lukoťka <relatko@gmail.com>
2024-10-29 13:34:08 +0100

    Add SimpleBuilding

--- stone_age/game_board/interfaces.py
+++ stone_age/game_board/interfaces.py
@@ -1,1 +1,8 @@
 # pylint: disable=unused-argument, duplicate-code
+from typing import Iterable, Optional
+from stone_age.simple_types import Effect
+
+
+class Building:
+    def build(self, resources: Iterable[Effect]) -> Optional[int]:
+        assert False
