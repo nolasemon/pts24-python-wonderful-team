684c4c04c5f0ed1a9700e9e63bce1199f1eb0ae7
scythehazker <voenkovv@mail.ru>
2024-11-17 10:31:19 +0100

    Implemented Throw class

---
+++ stone_age/game_board/throw.py
@@ -0,0 +1,13 @@
+from typing import List
+from random import randint
+
+
+class Throw:
+
+    minNumber: int = 1
+    maxNumber: int = 6
+
+    def throw(self, dices: int) -> List[int]:
+        assert isinstance(dices, int) and dices > 0
+        results: List[int] = [randint(self.minNumber, self.maxNumber) for dice in range(dices)]
+        return results
