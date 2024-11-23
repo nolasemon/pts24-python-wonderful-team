1bbac147929aedf386df2f05d08e14bca9f1831e
Zachar <zaharshch7@gmail.com>
2024-11-23 14:29:26 +0100

    Fixed mypy

--- test/game_board/building_tile_tests.py
+++ test/game_board/building_tile_tests.py
@@ -1,6 +1,4 @@
 import unittest
-from typing import Optional
-from unittest.mock import MagicMock
 from stone_age.game_board.building_tile import BuildingTile
 from stone_age.game_board.simple_types import Player
 from stone_age.game_board.arbitrary_building import ArbitraryBuilding
@@ -13,14 +11,14 @@ from stone_age.simple_types import Effect
 
 class TestBuildingTile(unittest.TestCase):
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: str, **kwargs: str) -> None:
         super().__init__(*args, **kwargs)
-        self.building_tile: Optional[BuildingTile] = None
+        self.building_tile: BuildingTile = BuildingTile(cards=[SimpleBuilding(
+            [Effect.CLAY, Effect.GOLD, Effect.STONE])
+            , ArbitraryBuilding(5), VariableBuilding(4, 2)])
 
     def set_up(self) -> None:
-        self.building_tile = BuildingTile(cards=[SimpleBuilding(
-            [Effect.CLAY,Effect.GOLD, Effect.STONE])
-            , ArbitraryBuilding(5), VariableBuilding(4, 2)])
+        pass
 
     def test_try_to_place_figures_success(self) -> None:
         player: Player = Player(PlayerOrder(0, 3), InterfacePlayerBoardGameBoard())
@@ -59,18 +57,18 @@ class TestBuildingTile(unittest.TestCase):
 
     def test_make_action_success(self) -> None:
         player  = Player(PlayerOrder(0, 4), InterfacePlayerBoardGameBoard())
+        assert self.building_tile is not None
         input_resources = [Effect.CLAY, Effect.GOLD, Effect.STONE]
         output_resources = [Effect.CLAY, Effect.GOLD, Effect.STONE]
-        assert self.building_tile is not None
         result = self.building_tile.make_action(player, input_resources, output_resources)
         self.assertEqual(result, ActionResult.ACTION_DONE)
 
     def test_make_action_failure_due_to_figures_present(self) -> None:
         player = Player(PlayerOrder(1, 4), InterfacePlayerBoardGameBoard())
+        assert self.building_tile is not None
         self.building_tile.figures = player.player_order.forward()
         input_resources = [Effect.CLAY, Effect.GOLD, Effect.STONE]
         output_resources = [Effect.CLAY, Effect.GOLD, Effect.STONE]
-        assert self.building_tile is not None
         result = self.building_tile.make_action(player, input_resources, output_resources)
         self.assertEqual(result, ActionResult.FAILURE)
 
@@ -82,67 +80,26 @@ class TestBuildingTile(unittest.TestCase):
         result = self.building_tile.make_action(player, input_resources, output_resources)
         self.assertEqual(result, ActionResult.FAILURE)
 
-    def test_make_action_failure_due_to_card_build_failure(self) -> None:
-        player = Player(PlayerOrder(2, 4), InterfacePlayerBoardGameBoard())
-        input_resources = [Effect.CLAY, Effect.GOLD,
-                           Effect.CLAY, Effect.GOLD, Effect.CLAY, Effect.GOLD]
-        output_resources = [Effect.CLAY, Effect.GOLD,
-                            Effect.CLAY, Effect.GOLD, Effect.CLAY, Effect.GOLD]
-        self.building_tile.cards[-1].build = MagicMock(return_value=None)
-        assert self.building_tile is not None
-        result = self.building_tile.make_action(player, input_resources, output_resources)
-        self.assertEqual(result, ActionResult.FAILURE)
-
-    def test_make_action_failure_due_to_try_to_make_action(self) -> None:
-        player = Player(PlayerOrder(2, 4), InterfacePlayerBoardGameBoard())
-        input_resources = [Effect(3)]
-        output_resources = [Effect(3)]
-        self.building_tile.try_to_make_action = MagicMock(
-            return_value=HasAction.NO_ACTION_POSSIBLE)
-        assert self.building_tile is not None
-        result = self.building_tile.make_action(player, input_resources, output_resources)
-        self.assertEqual(result, ActionResult.FAILURE)
-
-    def test_make_action_failure_due_to_take_resources(self) -> None:
-        player = Player(PlayerOrder(3, 4), InterfacePlayerBoardGameBoard())
-        input_resources = [Effect(2)]
-        output_resources = [Effect(2)]
-        assert self.building_tile is not None
-        player.player_board.take_resources = MagicMock(return_value=False)
-
-        result = self.building_tile.make_action(player, input_resources, output_resources)
-        self.assertEqual(result, ActionResult.FAILURE)
-
-    def test_make_action_success_removes_card(self) -> None:
-        player = Player(PlayerOrder(2, 4), InterfacePlayerBoardGameBoard())
-        input_resources = [Effect(5)]
-        output_resources = [Effect(5)]
-        assert self.building_tile is not None
-        # Ensure mock methods return valid success scenario
-        self.building_tile.cards[-1].build = MagicMock(return_value=6)
-        player.player_board.take_resources = MagicMock(return_value=True)
-
-        result = self.building_tile.make_action(player, input_resources, output_resources)
-        self.assertEqual(result, ActionResult.ACTION_DONE)
-        self.assertEqual(len(self.building_tile.cards), 0)
 
     def test_skip_action_success_no_figures(self) -> None:
         player = Player(PlayerOrder(2, 4), InterfacePlayerBoardGameBoard())
+        assert self.building_tile is not None
         self.building_tile.figures = None
         result = self.building_tile.skip_action(player)
         self.assertTrue(result)
 
     def test_skip_action_failure_figure_mismatch(self) -> None:
         player = Player(PlayerOrder(3, 4), InterfacePlayerBoardGameBoard())
-        self.building_tile.figures = PlayerOrder(0, 4)
         assert self.building_tile is not None
+        self.building_tile.figures = PlayerOrder(0, 4)
+
         result = self.building_tile.skip_action(player)
         self.assertFalse(result)
 
     def test_skip_action_success_remove_figure(self) -> None:
         player = Player(PlayerOrder(3, 4), InterfacePlayerBoardGameBoard())
-        self.building_tile.figures = player.player_order
         assert self.building_tile is not None
+        self.building_tile.figures = player.player_order
         result = self.building_tile.skip_action(player)
         self.assertTrue(result)
         self.assertIsNone(self.building_tile.figures)
@@ -150,26 +107,26 @@ class TestBuildingTile(unittest.TestCase):
     # Tests for try_to_make_action
     def test_try_to_make_action_no_action_player_order_mismatch(self) -> None:
         player = Player(PlayerOrder(0, 4), InterfacePlayerBoardGameBoard())
-        self.building_tile.figures = PlayerOrder(2, 4)
         assert self.building_tile is not None
+        self.building_tile.figures = PlayerOrder(2, 4)
         result = self.building_tile.try_to_make_action(player)
         self.assertEqual(result, HasAction.NO_ACTION_POSSIBLE)
 
     def test_try_to_make_action_no_action_possible_no_cards(self) -> None:
+        assert self.building_tile is not None
         self.building_tile.cards.clear()
         player = Player(PlayerOrder(0, 4), InterfacePlayerBoardGameBoard())
-        assert self.building_tile is not None
         self.building_tile.figures = player.player_order
         result = self.building_tile.try_to_make_action(player)
         self.assertEqual(result, HasAction.NO_ACTION_POSSIBLE)
 
     def test_try_to_make_action_no_action_possible_no_figures(self) -> None:
+        assert self.building_tile is not None
         self.building_tile = BuildingTile(cards=[SimpleBuilding(
             [Effect.CLAY, Effect.GOLD, Effect.STONE])
             , ArbitraryBuilding(5), VariableBuilding(4, 2)])
         player = Player(PlayerOrder(1, 4), InterfacePlayerBoardGameBoard())
         self.building_tile.figures = None
-        assert self.building_tile is not None
         result = self.building_tile.try_to_make_action(player)
         self.assertEqual(result, HasAction.NO_ACTION_POSSIBLE)
 
