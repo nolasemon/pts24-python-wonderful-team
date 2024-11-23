5f1caaca72f33b69746074f7d872db68412f6430
Timotej Šimkovič <tim24sim@gmail.com>
2024-11-23 19:10:31 +0100

    Edit of FeedTribeState class initalization

--- stone_age/game_phase_controller/feed_tribe_state.py
+++ stone_age/game_phase_controller/feed_tribe_state.py
@@ -1,32 +1,35 @@
 from __future__ import annotations
-from typing import Iterable
+from typing import Iterable, Mapping
 
 from stone_age.game_phase_controller.interfaces import GamePhaseStateFailureMeta
 from stone_age.interfaces import InterfaceFeedTribe
 from stone_age.simple_types import PlayerOrder, ActionResult, HasAction, Effect
 
 class FeedTribeState(GamePhaseStateFailureMeta):
-    _interface_feed_tribe: InterfaceFeedTribe
+    _dict_player_interface: Mapping[PlayerOrder, InterfaceFeedTribe]
 
-    def __init__(self, interface_feed_tribe: InterfaceFeedTribe):
-        self._interface_feed_tribe = interface_feed_tribe
+    def __init__(self, dict_player_interface: Mapping[PlayerOrder, InterfaceFeedTribe]):
+        """Initializing with argument dict_player_interface, 
+        where each player has their own interface for feeding the tribe.
+        When calling the particular methods, the right one is chosen."""
+        self._dict_player_interface = dict(dict_player_interface)
 
     def feed_tribe(self, player: PlayerOrder, resources: Iterable[Effect]) -> ActionResult:
         assert isinstance(player, PlayerOrder)
-        if self._interface_feed_tribe.feed_tribe(resources):
+        if self._dict_player_interface[player].feed_tribe(resources):
             return ActionResult.ACTION_DONE
         return ActionResult.FAILURE
 
     def do_not_feed_this_turn(self, player: PlayerOrder) -> ActionResult:
         assert isinstance(player, PlayerOrder)
-        if self._interface_feed_tribe.do_not_feed_this_turn():
+        if self._dict_player_interface[player].do_not_feed_this_turn():
             return ActionResult.ACTION_DONE
         return ActionResult.FAILURE
 
     def try_to_make_automatic_action(self, player: PlayerOrder) -> HasAction:
         assert isinstance(player, PlayerOrder)
-        if self._interface_feed_tribe.is_tribe_fed():
+        if self._dict_player_interface[player].is_tribe_fed():
             return HasAction.NO_ACTION_POSSIBLE
-        if self._interface_feed_tribe.feed_tribe_if_enough_food():
+        if self._dict_player_interface[player].feed_tribe_if_enough_food():
             return HasAction.AUTOMATIC_ACTION_DONE
         return HasAction.WAITING_FOR_PLAYER_ACTION
