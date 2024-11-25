ba2576d33b9b33b08cb53e2300b74fc1012f9ae7
Ján Chromík <jan.chromik92@gmail.com>
2024-11-25 19:38:50 +0100

    Changed argument in NewRoundState constructor - bug fix.

--- stone_age/game_phase_controller/new_round_state.py
+++ stone_age/game_phase_controller/new_round_state.py
@@ -8,19 +8,24 @@ from stone_age.simple_types import PlayerOrder, Location, HasAction
 
 
 class NewRoundState(GamePhaseStateFailureMeta):
-    """Uses also Mapping[Location, InterfaceFigureLocation], Iterable of
-    InterfaceFigureLocation in design makes no sense."""
+    """
+    Uses also Mapping[Location, InterfaceFigureLocation], Iterable of
+    InterfaceFigureLocation in design makes no sense.
+    Second argument in constructor is needed for new_turn method call
+    for each of the player boards.
+    """
     _places: Mapping[Location, InterfaceFigureLocation]
-    _new_turn: InterfaceNewTurn
+    _new_turn: Mapping[PlayerOrder, InterfaceNewTurn]
 
     def __init__(self, places: Mapping[Location, InterfaceFigureLocation],
-                 new_turn: InterfaceNewTurn):
+                 new_turn: Mapping[PlayerOrder, InterfaceNewTurn]):
         self._places = places
         self._new_turn = new_turn
 
     def try_to_make_automatic_action(self, player: PlayerOrder) -> HasAction:
         """NO_ACTION_POSSIBLE in NEW_ROUND game phase triggers GAME_END."""
-        self._new_turn.new_turn()
+        for player_board in self._new_turn:
+            self._new_turn[player_board].new_turn()
         for place in self._places:
             if self._places[place].new_turn():
                 return HasAction.NO_ACTION_POSSIBLE
