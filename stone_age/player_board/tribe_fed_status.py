eceb70cff36e8b5d976608259653143d38965fca
nolasemon <54684966+nolasemon@users.noreply.github.com>
2024-11-29 19:20:29 +0100

    Implement TribeFedStatus class and prepare tests

--- stone_age/player_board/tribe_fed_status.py
+++ stone_age/player_board/tribe_fed_status.py
@@ -1,27 +1,63 @@
-from typing import List
+from typing import List, Any
+import json
+
 from stone_age.interfaces import InterfaceGetState
 from stone_age.simple_types import Effect
+from stone_age.player_board.player_resources_and_food import PlayerResourcesAndFood
+from stone_age.player_board.player_figures import PlayerFigures
 
 
 class TribeFedStatus(InterfaceGetState):
-    def __init__(self) -> None:
-        self._tribe_fed = False
-        self._fields = 0
+    def __init__(
+        self,
+        resources_and_food: PlayerResourcesAndFood,
+        figures: PlayerFigures
+    ) -> None:
+        self._tribe_fed: bool = False
+        self._fields: int = 0
+        self._resources_and_food: PlayerResourcesAndFood = resources_and_food
+        self._figures: PlayerFigures = figures
+        # suppose feed methods can be called more than once in a round
+        self._fed_people: int = 0
+        self._new_fields: int = 0
 
     def add_field(self) -> None:
-        raise NotImplementedError
+        if self._fields < 10:
+            self._fields += 1
+            self._new_fields += 1
 
     def new_turn(self) -> None:
         self._tribe_fed = False
+        self._new_fields = 0
+        self._fed_people = 0
 
     def feed_tribe_if_enough_food(self) -> bool:
-        raise NotImplementedError
+        self._resources_and_food.take_resources(
+            self._new_fields * [Effect.FOOD])
+        self._new_fields = 0
+        to_feed_count: int = self._figures.get_total_figures - self._fed_people
+        necessary_food: List[Effect] = to_feed_count * [Effect.FOOD]
+        if not self._resources_and_food.has_resources(necessary_food):
+            return False
+        self._tribe_fed = True
+        self._resources_and_food.give_resources(necessary_food)
+        return True
 
     def feed_tribe(self, resources: List[Effect]) -> bool:
-        raise NotImplementedError
+        while self._fed_people < self._figures.get_total_figures and\
+                self._resources_and_food.has_resources([Effect.FOOD]):
+            self._fed_people += 1
+            self._resources_and_food.give_resources([Effect.FOOD])
+        remaining: int = self._figures.get_total_figures - self._fed_people
+        if len(resources) < remaining or not self._resources_and_food.has_resources(resources):
+            return False
+        self._resources_and_food.give_resources(resources)
+        self._tribe_fed = True
+        return True
 
     def set_tribe_fed(self) -> bool:
-        raise NotImplementedError
+        self._tribe_fed = True
+        return True
 
     def is_tribe_fed(self) -> bool:
         return self._tribe_fed
@@ -31,4 +67,8 @@ class TribeFedStatus(InterfaceGetState):
         return self._fields
 
     def state(self) -> str:
-        raise NotImplementedError
+        state: Any = {
+            "tribe fed": self._tribe_fed,
+            "fields": self._fields,
+        }
+        return json.dumps(state)
---
+++ test/player_board/test_tribe_fed_status.py
@@ -0,0 +1,172 @@
+import unittest
+import json
+from typing import Any
+
+from stone_age.simple_types import Effect
+from stone_age.player_board.player_figures import PlayerFigures
+from stone_age.player_board.player_resources_and_food import PlayerResourcesAndFood
+from stone_age.player_board.tribe_fed_status import TribeFedStatus
+
+
+class TestTribeFedStatus(unittest.TestCase):
+    def setUp(self) -> None:
+        self.resources_and_food = PlayerResourcesAndFood()
+        self.figures = PlayerFigures(figures_start=10)
+        self.fed_status = TribeFedStatus(self.resources_and_food, self.figures)
+
+    def test_state(self) -> None:
+        s1: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s1, {"tribe fed": False, "fields": 0})
+
+        self.fed_status._fields = 1337  # pylint: disable=protected-access
+        self.fed_status._tribe_fed = True  # pylint: disable=protected-access
+        s2: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s2, {"tribe fed": True, "fields": 1337})
+
+    def test_no_food(self) -> None:
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertFalse(self.fed_status.feed_tribe([]))
+        self.assertTrue(self.fed_status.set_tribe_fed())
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 0})
+
+    def test_add_field(self) -> None:
+        self.fed_status.add_field()
+        s1: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s1, {"tribe fed": False, "fields": 1})
+
+        self.fed_status.add_field()
+        self.fed_status.add_field()
+        s2: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s2, {"tribe fed": False, "fields": 3})
+
+        for _ in range(100):
+            self.fed_status.add_field()
+        s3: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s3, {"tribe fed": False, "fields": 10})
+
+    def test_feed_if_enough_food_success(self) -> None:
+        self.resources_and_food.take_resources([Effect.FOOD] * 10)
+        self.assertTrue(self.fed_status.feed_tribe_if_enough_food())
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 0})
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+
+    def test_feed_tribe_with_resources(self) -> None:
+        self.resources_and_food.take_resources([Effect.FOOD] * 8)
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertFalse(self.fed_status.feed_tribe(
+            [Effect.WOOD, Effect.CLAY]))
+        self.resources_and_food.take_resources([Effect.WOOD, Effect.CLAY])
+        self.assertTrue(self.fed_status.feed_tribe([Effect.WOOD, Effect.CLAY]))
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 0})
+
+    def test_feed_tribe_unsuccessful(self) -> None:
+        self.resources_and_food.take_resources([Effect.FOOD] * 8)
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertFalse(self.fed_status.feed_tribe([]))
+        self.assertTrue(self.fed_status.set_tribe_fed())
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+        s1: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s1, {"tribe fed": True, "fields": 0})
+
+        self.fed_status.new_turn()
+
+        self.resources_and_food.take_resources(
+            [Effect.FOOD] * 7 + [Effect.CLAY, Effect.GOLD])
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertFalse(self.fed_status.feed_tribe(
+            [Effect.CLAY, Effect.GOLD]))
+        self.assertTrue(self.fed_status.set_tribe_fed())
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+        s2: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s2, {"tribe fed": True, "fields": 0})
+
+    def test_multiple_calls_food(self) -> None:
+        self.resources_and_food.take_resources([Effect.FOOD] * 5)
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertFalse(self.fed_status.feed_tribe([]))
+        s1: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s1, {"tribe fed": False, "fields": 0})
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+
+        self.resources_and_food.take_resources([Effect.FOOD] * 5)
+        self.assertTrue(self.fed_status.feed_tribe_if_enough_food())
+        s2: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s2, {"tribe fed": True, "fields": 0})
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+
+    def test_feed_with_fields(self) -> None:
+        self.resources_and_food.take_resources([Effect.FOOD] * 8)
+        self.fed_status.add_field()
+        self.fed_status.add_field()
+        self.assertTrue(self.fed_status.feed_tribe_if_enough_food())
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 2})
+
+    def test_new_turn(self) -> None:
+        self.resources_and_food.take_resources([Effect.FOOD] * 8)
+        self.fed_status.add_field()
+        self.fed_status.add_field()
+        self.fed_status.feed_tribe_if_enough_food()
+
+        self.fed_status.new_turn()
+
+        for _ in range(10):
+            self.fed_status.add_field()
+
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.resources_and_food.take_resources([Effect.CLAY, Effect.GOLD])
+        self.assertTrue(self.fed_status.feed_tribe([Effect.CLAY, Effect.GOLD]))
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 10})
+
+    def test_surplus_food(self) -> None:
+        for _ in range(5):
+            self.fed_status.add_field()
+        self.resources_and_food.take_resources(
+            [Effect.FOOD] * 11 + [Effect.CLAY] * 5)
+        self.assertTrue(self.fed_status.feed_tribe_if_enough_food())
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 5})
+        self.assertTrue(
+            self.resources_and_food.has_resources([Effect.FOOD] * 6))
+        self.assertFalse(
+            self.resources_and_food.has_resources([Effect.FOOD] * 7))
+        self.assertTrue(
+            self.resources_and_food.has_resources([Effect.CLAY] * 5))
+        self.assertFalse(
+            self.resources_and_food.has_resources([Effect.CLAY] * 6))
+
+    def test_surplus_resources(self) -> None:
+        for _ in range(5):
+            self.fed_status.add_field()
+
+        self.resources_and_food.take_resources([Effect.GOLD] * 6)
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertTrue(self.fed_status.feed_tribe([Effect.GOLD] * 5))
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 5})
+        self.assertTrue(self.resources_and_food.has_resources([Effect.GOLD]))
+        self.assertFalse(
+            self.resources_and_food.has_resources([Effect.GOLD] * 2))
+
+    def test_surplus_both(self) -> None:
+        for _ in range(5):
+            self.fed_status.add_field()
+
+        self.resources_and_food.take_resources(
+            [Effect.FOOD] * 4 + [Effect.CLAY] * 3)
+        self.assertFalse(self.fed_status.feed_tribe_if_enough_food())
+        self.assertTrue(self.fed_status.feed_tribe([Effect.CLAY]))
+        s: Any = json.loads(self.fed_status.state())
+        self.assertEqual(s, {"tribe fed": True, "fields": 5})
+        self.assertFalse(self.resources_and_food.has_resources([Effect.FOOD]))
+        self.assertTrue(
+            self.resources_and_food.has_resources([Effect.CLAY] * 2))
+        self.assertFalse(
+            self.resources_and_food.has_resources([Effect.CLAY] * 3))
