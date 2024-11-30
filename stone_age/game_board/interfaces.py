ff56f03b7ec98fb91dc979d2ef81dc810992e04d
Ján Chromík <120592002+quendingoldo@users.noreply.github.com>
2024-11-30 18:41:40 +0100

    Update interfaces.py
    Amended git's wrong automatic merge.

--- stone_age/game_board/interfaces.py
+++ stone_age/game_board/interfaces.py
@@ -7,37 +7,28 @@ from stone_age.simple_types import Effect, HasAction, ActionResult, PlayerOrder
 class Building:
 
     def build(self, resources: Iterable[Effect]) -> Optional[int]:
-        assert all(Effect.is_resource(x) for x in resources)
-        return sum(Effect.points(x) for x in resources)
-
+        assert False
 
 class InterfaceFigureLocationInternal:
 
     def place_figures(self, player: Player, figure_count: int) -> bool:
-        assert isinstance(player, Player) and figure_count > 0
-        return False
+        assert False
 
     def try_to_place_figures(self, player: Player, count: int) -> HasAction:
-        assert isinstance(player, Player) and count > 0
-        return HasAction.NO_ACTION_POSSIBLE
+        assert False
 
     def make_action(self, player: Player, input_resources: Iterable[Effect],
                     output_resources: Iterable[Effect]) -> ActionResult:
-        assert isinstance(player, Player)
-        assert isinstance(input_resources, Iterable) and isinstance(
-            output_resources, Iterable)
-        return ActionResult.FAILURE
+        assert False
 
     def skip_action(self, player: Player) -> bool:
-        assert isinstance(player, Player)
-        return False
+        assert False
 
     def try_to_make_action(self, player: Player) -> HasAction:
-        assert isinstance(player, Player)
-        return HasAction.NO_ACTION_POSSIBLE
+        assert False
 
     def new_turn(self) -> bool:
-        return False
+        assert False
 
 
 class EvaluateCivilizationCardImmediateEffect:
