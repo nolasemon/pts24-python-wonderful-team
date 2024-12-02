08bfae5201c5ba19846d108a0f8bcbee51ecec8d
nolasemon <54684966+nolasemon@users.noreply.github.com>
2024-12-02 23:50:41 +0100

    Fix formatting

--- stone_age/stone_age_game.py
+++ stone_age/stone_age_game.py
@@ -25,12 +25,14 @@ class StoneAgeGame(InterfaceStoneAgeGame):
     _phase_controller: InterfaceGamePhaseController
     _observable: StoneAgeObservable
 
-    def __init__(self, players: Mapping[int, PlayerOrder],
-                 player_boards: Mapping[PlayerOrder, InterfaceGetState],
-                 game_board: InterfaceGetState,
-                 phase_controller: InterfaceGamePhaseController,
-                 observable: StoneAgeObservable
-                 ) -> None:
+    def __init__(
+        self,
+        players: Mapping[int, PlayerOrder],
+        player_boards: Mapping[PlayerOrder, InterfaceGetState],
+        game_board: InterfaceGetState,
+        phase_controller: InterfaceGamePhaseController,
+        observable: StoneAgeObservable
+    ) -> None:
         self._players = players
         self._player_boards = player_boards
         self._game_board = game_board
@@ -46,64 +48,87 @@ class StoneAgeGame(InterfaceStoneAgeGame):
         self._state = json.dumps({
             "player_board " + str(self._players[i]):
                 json.loads(self._player_boards[self._players[i]].state())
-                for i in sorted(self._players)
-        } | {"game_board": json.loads(self._game_board.state()),
-             "game_phase": json.loads(self._phase_controller.state())}
-        )
+            for i in sorted(self._players)
+        } | {
+            "game_board": json.loads(self._game_board.state()),
+            "game_phase": json.loads(self._phase_controller.state())
+        })
 
     def place_figures(self, player_id: int, location: Location, figures_count: int) -> bool:
-        output: bool = self._phase_controller.place_figures(self._players[player_id],
-                                                            location, figures_count)
+        output: bool = self._phase_controller.place_figures(
+            self._players[player_id],
+            location,
+            figures_count
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
-    def make_action(self, player_id: int, location: Location,
-                    used_resources: Iterable[Effect], desired_resource: Iterable[Effect]) -> bool:
-        output: bool = self._phase_controller.make_action(self._players[player_id], location,
-                                                          used_resources, desired_resource)
+    def make_action(
+        self,
+        player_id: int,
+        location: Location,
+        used_resources: Iterable[Effect],
+        desired_resource: Iterable[Effect]
+    ) -> bool:
+        output: bool = self._phase_controller.make_action(
+            self._players[player_id],
+            location,
+            used_resources,
+            desired_resource
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
     def skip_action(self, player_id: int, location: Location) -> bool:
         output: bool = self._phase_controller.skip_action(
-            self._players[player_id], location)
+            self._players[player_id],
+            location
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
     def use_tools(self, player_id: int, tool_index: int) -> bool:
         output: bool = self._phase_controller.use_tools(
-            self._players[player_id], tool_index)
+            self._players[player_id],
+            tool_index
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
     def no_more_tools_this_throw(self, player_id: int) -> bool:
         output: bool = self._phase_controller.no_more_tools_this_throw(
-            self._players[player_id])
+            self._players[player_id]
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
     def feed_tribe(self, player_id: int, resources: Iterable[Effect]) -> bool:
         output: bool = self._phase_controller.feed_tribe(
-            self._players[player_id], resources)
+            self._players[player_id],
+            resources
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
     def do_not_feed_this_turn(self, player_id: int) -> bool:
         output: bool = self._phase_controller.do_not_feed_this_turn(
-            self._players[player_id])
+            self._players[player_id]
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
 
     def make_all_players_take_a_reward_choice(self, player_id: int, reward: Effect) -> bool:
         output: bool = self._phase_controller.make_all_players_take_a_reward_choice(
-            self._players[player_id], reward)
+            self._players[player_id],
+            reward
+        )
         self._update_state()
         self._observable.notify(self._state)
         return output
